35dff24395ceb82fed02ae9729f7bd8f8a399139
GideonBature <infoaboutgideon@gmail.com>
2024-03-14 13:31:24 +0100

    basic annotation - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""type annoted function concat
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """concat function
+    args:
+        str1: string
+        str2: string
+    returns:
+        string
+    """
+    return str1 + str2
