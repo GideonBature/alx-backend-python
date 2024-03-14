671b1d7f5075270709f8639a029527c2385af761
GideonBature <infoaboutgideon@gmail.com>
2024-03-14 18:59:58 +0100

    add docstring

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -5,6 +5,12 @@ from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """type annotated function
+    args:
+        multiplier: float
+    return:
+        Callable[[float], float]
+    """
 
     def inner(x: float) -> float:
         return x * multiplier
