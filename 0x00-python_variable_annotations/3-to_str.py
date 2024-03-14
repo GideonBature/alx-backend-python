faf07aae5566145bd048df9f790a26c81d6b18dc
GideonBature <infoaboutgideon@gmail.com>
2024-03-14 13:45:37 +0100

    basic annotation - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Basic annotations - to string
+"""
+
+
+def to_str(n: float) -> str:
+    """type-annotated function
+    args:
+        n: float
+    return:
+        str
+    """
+    return str(n)
