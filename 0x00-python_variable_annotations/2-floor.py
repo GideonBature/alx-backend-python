44b29d7012ccb8af628d3aa6f8a8e2dba6c1ae18
GideonBature <infoaboutgideon@gmail.com>
2024-03-14 13:37:08 +0100

    basic annotation - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""basic annotations - floor
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """floor function
+    args:
+        n: floor
+    returns:
+        int
+    """
+    return math.floor(n)
