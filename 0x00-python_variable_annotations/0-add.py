9f09f66e232196793858c83d3077808ac85559d1
GideonBature <infoaboutgideon@gmail.com>
2024-03-14 13:26:17 +0100

    basic annotation - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Type annotated function
+"""
+
+
+def add(a: float, b: float) -> float:
+    """add function
+    args:
+        a: float
+        b: float
+    returns:
+        a + b: float
+    """
+    return a + b
