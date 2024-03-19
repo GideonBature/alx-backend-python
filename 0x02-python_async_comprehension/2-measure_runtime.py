2ee95a634ac800e211696c6971869520975cce9a
GideonBature <infoaboutgideon@gmail.com>
2024-03-19 18:00:01 +0100

    add variable annotation

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -7,7 +7,7 @@ import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime():
+async def measure_runtime() -> float:
     """Measure runtime
     """
     start = time.perf_counter()
