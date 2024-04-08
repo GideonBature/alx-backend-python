06cd765884f9aba134a73b36097e3758821776d5
GideonBature <infoaboutgideon@gmail.com>
2024-04-08 20:19:42 +0100

    5. Mocking a property

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,7 +2,7 @@
 """4. Parameterize and patch as decorators
 """
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 
@@ -26,3 +26,17 @@ class TestGithubOrgClient(unittest.TestCase):
         client = GithubOrgClient(org_name)
         self.assertEqual(client.org, expected_org)
         mock_get_json.assert_called_once_with(url)
+
+    @patch('client.GithubOrgClient.org', new_callable=PropertyMock)
+    def test_public_repos_url(self, mock_org):
+        """Test that GithubOrgClient._public_repos_url
+        returns the correct JSON response.
+        """
+        url = "https://api.github.com/orgs/google/repos"
+        mock_org.return_value = {"repos_url": url}
+
+        client = GithubOrgClient("google")
+        expected_url = "https://api.github.com/orgs/google/repos"
+        self.assertEqual(client._public_repos_url, expected_url)
+
+        mock_org.assert_called_once()
